6cdf8362065a45613fe285f12755ddfa4ca4149d
Mateus Ceni <mateus.ceni@gmail.com>
2023-04-12 19:16:38 -0300

    aula terraform

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Set the variable value in *.tfvars file
+# or using -var="do_token=..." CLI option
+variable "do_token" {}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = "dop_v1_e384d0a6778c64846e4fe1c9edd1a31fc4d03477b083a7699982c7521cef0b2b"
+}
+
+
+# Create a new Web Droplet in the nyc2 region
+resource "digitalocean_droplet" "jenkins" {
+  image  = "ubuntu-22-04-x64"
+  name   = "jenkins"
+  region = "nyc1"
+  size   = "s-2vcpu-2gb"
+  ssh_keys = [digitalocean_ssh_key.jornada.fingerprint]
+}
+
+resource "digitalocean_ssh_key" "jornada" {
+  name       = "jornada"
+}
+
